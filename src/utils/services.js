a37a29c8d17edfd132844f101ae5ec74feef78b0
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-02-06 09:49:18 +0530

    new vercel server added

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://doctor-data-6oen.onrender.com";
+// export const BASE_URL = "https://doctor-data-6oen.onrender.com";
+export const BASE_URL = "https://doctor-api-server.vercel.app";
